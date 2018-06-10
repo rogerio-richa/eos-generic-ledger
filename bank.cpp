9cff4f38c1f59856db1b87de220d00613aeae492
Rogerio Richa <rogerio.richa@cellcrypt.com>
2018-06-10 02:10:20 -0300

    Making _ledger_stats into _ledger_config and turning it into a singleton

--- bank.cpp
+++ bank.cpp
@@ -1,5 +1,6 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/print.hpp>
+#include <eosiolib/singleton.hpp>
 
 using namespace eosio;
 using namespace std;
@@ -11,65 +12,54 @@ class bank : public contract {
         bank( account_name self ) :
             contract(self),
             _ledger(_self, N(ledger)),
-            _ledger_stats(_self, N(ledger))
+            _ledger_config(_self, N(ledger))
     {}
 
         // @abi action
         void submit(name sender, string note) {
             
             require_auth(sender);
-            
             // add here a check to only allow a specific user to manipulate this ledger
 
-            auto iter = _ledger.find(0);
-            if(iter == _ledger.end()) {
+            if (!_ledger_config.exists()) {
                 
+                print("here");
+                _ledger_info info{};
+                info.height = 0;
+
+                _ledger_config.set(info, _self); // what is _self? TO DO - read doc
+
                 _ledger.emplace( sender, [&] (auto& row) {
-                    
+
                     row.height = 0;
                     row.note = note;
-                });
-                
-                _ledger_stats.emplace( sender, [&](auto& row) {
-
-                    row.id = N(ledger_depth);
-                    row.height = 0;
                 });
                 
                 return;
             }
             
-            auto iter2 = _ledger_stats.find(N(ledger_depth));
-            uint64_t height = (iter2->height);
+            _ledger_info info{};
+            info = _ledger_config.get();
+            info.height++;
+            _ledger_config.set(info, _self);
             
+            uint64_t height = (info.height);
+            print(height);
+
             _ledger.emplace( sender, [&](auto& row) {
 
-                row.height = ++height;
+                row.height = height;
                 row.note = note;
-            });
-            
-            
-            _ledger_stats.modify (iter2, sender, [&](auto& row) {
-                
-                row.height++;
             });
         }
 
     private:
     
-        // @abi table ledgerstats
         struct _ledger_info {
 
-            uint64_t id;
             uint64_t height;
-            uint64_t primary_key() const {
-
-                return id;
-            }
-
-            EOSLIB_SERIALIZE(_ledger_info, (id)(height))
         };
-        multi_index<N(ledgerstats), _ledger_info> _ledger_stats;
+        singleton<N(ledgerconf), _ledger_info> _ledger_config;
 
     
         // @abi table ledger
@@ -84,7 +74,6 @@ class bank : public contract {
             
             EOSLIB_SERIALIZE(_ledger_entry, (height)(note))
         };
-
         multi_index<N(ledger), _ledger_entry> _ledger;
 };
 
